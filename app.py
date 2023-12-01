0c08f20cbd902d190ca7fa90510297a6d223b0da
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-12-01 14:52:34 -0330

    Index and forms

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, FloatField, StringField
+from wtforms.validators import DataRequired
+import numpy as np
+
+class MpgForm(FlaskForm):
+    make = StringField('Make', validators=[DataRequired()])
+    model = StringField('Model', validators=[DataRequired()])
+    submit = SubmitField('Predict MPG')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key' #TODO: replace with secret key
 
 @app.route('/')
 def home():
